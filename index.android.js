13c321ec36141a20a357071f3113dc5a03f4dcea
Qiao Liang <qiaoliang89@gmail.com>
2015-11-03 00:03:38 +0800

    fix socketio

--- index.android.js
+++ index.android.js
@@ -12,26 +12,49 @@ var {
   View,
 } = React;
 var digestAuthRequest = require('./digestAuthRequest');
+window.navigator.userAgent = 'react-native'
+var io = require('socket.io-client/socket.io') ;
+
 var RNMenu = React.createClass({
   getInitialState: function() {
     return {
-      text: 'start!!'
+      text: 'posGuid=ghi!xx!'
     }
   },
   componentDidMount: function() {
+    var self = this;
     var url = 'http://104.155.205.124/auth/login?posGuid=abc';
     var uri = '/auth/login?posGuid=abc';
-    var self = this;
+
     var req = new digestAuthRequest('GET', url, uri, '7737', '7737');
     // make the request
     req.request(function(data) {
         console.log('Data retrieved successfully');
         console.log(data);
-        self.setState({text: JSON.stringify(data)});
+        //self.setState({text: JSON.stringify(data)});
         console.log('Above is the retrieved');
     },function(errorCode) {
         console.log('no dice: '+errorCode);
     }, {});
+
+    var socket = io.connect('http://104.155.205.124', {query: "posGuid=ghi"});
+    socket.on('connect', function(){ self.setState ({text:"connected!"});});
+    socket.on('connect_error', function(data){
+      self.setState({text:"connect error:" + JSON.stringify(data)});
+    });
+    socket.on('permission', function(data){self.setState({text:"permission event occurred: " + JSON.stringify(data)})});
+    //socket.on('order', function(data){console.log ("order event occurred: " + JSON.stringify(data))});
+    socket.on('order', function(data){self.setState ({text:"order event occurred: " + JSON.stringify(data)})});
+    socket.on('orderitem', function(data){self.setState({text:"orderItem event occurred: " + JSON.stringify(data)})});
+    //socket.get('/pos', function(resData, jwres) {console.log("ResData: " + resData);})
+    // var permissionGetReq = '/order?webToken=' + webToken;
+    // socket.emit('post', {"url": permissionGetReq}, function (res) { console.log('response from post: ' + JSON.stringify(res));});
+    socket.on('connection', function(socket) {
+        socket.conn.on('heartbeat', function() {
+          console.log('heartbeat');
+        });
+    });
+
   },
   render: function() {
     return (
