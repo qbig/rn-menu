172170c86ca053463952582e902d4771e103015e
Qiao Liang <qiaoliang89@gmail.com>
2016-01-21 17:04:33 +0800

    added sentry to ios

--- index.android.js
+++ index.android.js
@@ -1,7 +1,14 @@
 'use strict';
 
 var React = require('react-native');
+var Raven = require('raven-js');
+require('raven-js/plugins/react-native')(Raven);
+Raven
+  .config('https://2e17bf8efad3484885d58ae04608a468@app.getsentry.com/63290', { release: "1.1.3" })
+  .install();
+
 require('regenerator/runtime');
+
 var {
   AppRegistry
 } = React;
--- index.ios.js
+++ index.ios.js
@@ -1,11 +1,17 @@
 'use strict';
 
 var React = require('react-native');
+var Raven = require('raven-js');
+require('raven-js/plugins/react-native')(Raven);
+Raven
+  .config('https://04a856d057544c3d8ec5b2a94b82efe6@app.getsentry.com/64445', { release: "" })
+  .install();
+
 require('regenerator/runtime');
+
 var {
   AppRegistry
 } = React;
 
 var Root = require('./Screens/Root');
-
 AppRegistry.registerComponent('RNMenu', () => Root);
